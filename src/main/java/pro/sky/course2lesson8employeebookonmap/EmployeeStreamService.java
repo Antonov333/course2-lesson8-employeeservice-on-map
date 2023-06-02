23b28f1f9ab629788369b157928ba4631507317d
Antonov333 <antonovsergei333@yahoo.com>
2023-06-02 18:51:28 +0300

    Generally, homework is completed

--- src/main/java/pro/sky/course2lesson8employeebookonmap/EmployeeStreamService.java
+++ src/main/java/pro/sky/course2lesson8employeebookonmap/EmployeeStreamService.java
@@ -31,8 +31,6 @@ public class EmployeeStreamService {
         return employeeList;
     }
 
-    // !! make method to hire some exmaple employees
-
     public Employee hire(String firstName, String lastName, int deptId, int salary) {
 
         NameCheck nameCheck = new NameCheck(firstName, lastName);
@@ -64,26 +62,34 @@ public class EmployeeStreamService {
     public List<Employee> exampleHiring() {
         Random r = new Random();
         hire("John", "Smith", 5, r.nextInt(50000, 100000));
-        hire("Pamela", "Anderson", 5, r.nextInt(50000, 100000));
-        hire("Tommy", "Lee", 5, r.nextInt(50000, 100000));
+
         hire("Till", "Shweiger", 5, r.nextInt(50000, 100000));
         hire("Ann", "Brown", 2, r.nextInt(50000, 100000));
         hire("Ron", "Grownship", 2, r.nextInt(50000, 100000));
         hire("James", "Alarmson", 2, r.nextInt(50000, 100000));
+        hire("Pamela", "Anderson", 5, r.nextInt(50000, 100000));
+        hire("Tommy", "Lee", 5, r.nextInt(50000, 100000));
         return employeeList;
 
     }
 
     private boolean isHired(Employee employee) {
-        boolean result = employeeList.contains(employee);
-        return result;
+        return employeeList.contains(employee);
     }
 
     public String welcome() {
         return "<h2> Course 2, Lesson 9: Stream and Optional<br><br></h2>"
-
-                + "Under construction, sorry<br><br><a href=\"http://localhost:8080/departments/\"> Departments </a>";
-        // !! make hint for /deparments/max-salary and other commands !!
+                + "<br><br><a href=\"http://localhost:8080/departments/\"> Departments </a><br><br>" +
+                "<a href=\"http://localhost:8080/departments/example-hire/\"> Hire example persons </a><br>" +
+                "<a href=\"http://localhost:8080/departments/max-salary?departmentId=2\"> Max Salary in Department No.2 </a> |" +
+                "<a href=\"http://localhost:8080/departments/max-salary?departmentId=5\"> Max Salary in Department No.5</a> |" +
+                "<a href=\"http://localhost:8080/departments/max-salary\"> Max Salary in Company </a><br>" +
+                "<a href=\"http://localhost:8080/departments/min-salary?departmentId=2\"> Min Salary in Department No.2 </a> |" +
+                "<a href=\"http://localhost:8080/departments/min-salary?departmentId=5\"> Min Salary in Department No.5 </a> |" +
+                "<a href=\"http://localhost:8080/departments/min-salary\"> Min Salary in Company </a><br>" +
+                "<a href=\"http://localhost:8080/departments/all\"> Company crew sorted by department </a> |" +
+                "<a href=\"http://localhost:8080/departments/all?departmentId=2\"> Department No.2 crew </a> |" +
+                "<a href=\"http://localhost:8080/departments/all?departmentId=5\"> Department No.5 crew </a>";
     }
 
     public String maxSalary(Integer deptId) {
@@ -93,10 +99,8 @@ public class EmployeeStreamService {
             return "no personnel hired";
         }
 
-        List<Integer> salaries = new ArrayList<>();
-        salaries = deptCrewResult.stream().map(employee -> employee.getSalary()).collect(Collectors.toList());
-        salaries.sort(Comparator.reverseOrder());
-        return salaries.get(0) + " --- " + deptCrewResult;
+        deptCrewResult.sort(Comparator.comparingInt(employee -> employee.getSalary()));
+        return deptCrewResult.get(deptCrewResult.size() - 1).getSalary() + "<br><br>" + deptCrewResult;
     }
 
     public String minSalary(Integer deptId) {
@@ -107,16 +111,15 @@ public class EmployeeStreamService {
             return "no personnel hired";
         }
         deptCrewResult.sort(Comparator.comparingInt(employee -> employee.getSalary()));
-        return "! " + deptCrewResult.get(0) + " ! " deptCrewResult.toString();
+        return deptCrewResult.get(0).getSalary() + "<br><br>" + deptCrewResult.toString();
 
     }
 
     public List<Employee> getDeptCrew(Integer dept) {
 
         if (dept == null) {
-            List<Employee> crewSortedByDept = new ArrayList<>();
+            List<Employee> crewSortedByDept;
             crewSortedByDept = employeeList.stream().sorted().collect(Collectors.toList());
-            ;
             return crewSortedByDept;
         }
         List<Employee> deptCrew = employeeList.stream().filter(employee -> {
@@ -125,88 +128,5 @@ public class EmployeeStreamService {
                 collect(Collectors.toList());
         return deptCrew;
     }
-/**
- public void printDeptCrews() {
- System.out.println("List of personnel by departments");
- int i = 0;
- for (; i < depts.length; i++) {
-
- System.out.println(depts[i].toString());
- for (int j = 0; j < team.length; j++) {
- if (team[j] != null) {
- if (team[j].getDeptId() == i) {
- System.out.println(team[j].getPerson());
- }
- }
- }
-
- }
- System.out.println();
- }
-
- public int getMinSalary() {
- int s = 0;
- boolean firstIteration = true;
- for (Employee employee : team) {
- if (employee != null) {
- if (s > employee.getSalary() || firstIteration) {
- s = employee.getSalary();
- firstIteration = false;
- }
- }
- }
- return s;
- }
-
- public void printMinSalary() {
- int min = getMinSalary();
- System.out.println("Minimum salary of " + min + " earned by");
- for (Employee employee : team) {
- if ((employee != null) && employee.getSalary() == min) {
- System.out.println(employee);
- }
- }
- System.out.println();
- }
-
- public int getMaxSalary() {
- int max = 0;
- for (Employee employee : team) {
- if (employee != null) {
- if (max < employee.getSalary()) {
- max = employee.getSalary();
- }
- }
- }
- return max;
- }
-
- public void printMaxSalary() {
- int max = getMaxSalary();
- System.out.println("Maximum salary of " + max + " earned by");
- for (Employee employee : team) {
- if (employee != null) {
- if (employee.getSalary() == max) {
- System.out.println(employee);
- }
- }
- }
- System.out.println();
- }
-
- public int getMaxSalaryPosition() {
- int max = getMaxSalary();
- int p = 0;
- int i = 0;
- for (; i < team.length; i++) {
- if (team[i] != null) {
- if (team[i].getSalary() == max) {
- p = i + 1;
- return p;
- }
- }
- }
- return p;
- } */
 
 }
